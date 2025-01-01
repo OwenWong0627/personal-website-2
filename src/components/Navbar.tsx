ec9f0346f1c0609b66ad85249c69710dea713050
Owen Wong <owen.wong0627@gmail.com>
2025-01-01 06:17:44 -0500

    Feat: Enhance navbar interaction

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,26 @@
+import React, { useState } from "react";
 import AnimatedLink from "./NavbarLink";
 import BackButton from "./BackButton";
 import { useActiveLink } from "@/contexts/ActiveLinkContext";
 
-// interface NavbarProps {
-//     controlsRef: React.RefObject<OrbitControlsImpl>;
-// }
-
 const Navbar: React.FC = () => {
     const { isNavbarRetracted } = useActiveLink();
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <>
             <BackButton />
             <div
-                className={`absolute top-0 left-0 right-0 bg-gradient-to-r from-pastel-pink to-pastel-blue py-6 z-10 transition-all duration-300 ${
-                    isNavbarRetracted ? "-translate-y-full" : "translate-y-0"
+                className={`absolute top-0 left-0 right-0 bg-gradient-to-r from-pastel-pink to-pastel-blue transition-all duration-700 ${
+                    isNavbarRetracted && !isHovered
+                        ? "-translate-y-[90%]"
+                        : "translate-y-0"
                 }`}
+                style={{ zIndex: 2000000000 }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
             >
-                <nav className="flex justify-around text-base-plus font-semi-bold tracking-tightest font-switzer">
+                <nav className="flex justify-around text-base-plus font-semi-bold tracking-tightest font-switzer py-6">
                     <AnimatedLink href="/about-me">About Me</AnimatedLink>
                     <AnimatedLink href="/experience">Experience</AnimatedLink>
                     <AnimatedLink href="/projects">Projects</AnimatedLink>
